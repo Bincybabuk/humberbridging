65dd6537e33d781b69252122794c5fcadd1dd1df
unknown <bincy16@gmail.com>
2019-11-07 20:49:16 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace humberbridging
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int sum = 0;
+                Console.WriteLine("program for adding the string of array");
+                Console.WriteLine("insert string");
+                string array = Console.ReadLine();
+                string[] sumarray = array.Split('+');
+
+
+                for (int i = 0; i < sumarray.Length; i++)
+                {
+                    sum += int.Parse(sumarray[i]);
+
+
+
+
+                }
+                Console.WriteLine("sum of array{0}", sum);
+                Console.WriteLine($"sum of array{sum}");
+            }
+            catch
+                {
+                Console.WriteLine("wrong input");
+            
+            
+            
+            
+            }
+
+        }
+    }
+}
